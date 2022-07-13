570e19fdbf7c62808de8d82c37dc73e3ba0aed1a
Benjamin Luo <benjaminpluo2001@gmail.com>
2022-07-13 08:01:20 -0700

    delete

--- server/app.js
+++ server/app.js
@@ -12,20 +12,40 @@ app.use(express.urlencoded({ extended : false}));
 
 // create
 app.post('/insert', (request, response) => {
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
 
+    const result = db.insertNewName(name);
+
+    result
+    .then(data => response.json({ data : data}))
+    .catch(err => console.log(err));
 });
 
 // read
 app.get('/getAll', (request, response) => {
     const db = dbService.getDbServiceInstance();
     
-    const  result = db.getAllData();
+    const result = db.getAllData();
     
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
 })
 
 // update
 
 
 // delete
+app.delete('/delete/:id', (request, response) => {
+    const { id } = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.deleteRowById(id);
+
+    result
+    .then(data => response.json({success:data}))
+    .catch(error => console.log(err));
+})
 
 app.listen(process.env.PORT, () => console.log('app is running'));
--- server/dbService.js
+++ server/dbService.js
@@ -15,7 +15,7 @@ connection.connect((err) => {
     if(err) {
         console.log(err.message);
     }
-    console.log('db ' + connection.state);
+    // console.log('db ' + connection.state);
 })
 
 class DbService {
@@ -32,11 +32,53 @@ class DbService {
                     resolve(results);
                 })
             });
+            // console.log(response);
+            return response;
 
-            console.log(response);
         } catch (error) {
             console.log(error);
         }
+    }
+    
+    async insertNewName(name) {
+        try {
+            const dateAdded = new Date();
+            const insertId = await new Promise((resolve, reject) => {
+                const query = "INSERT INTO names (name, date_added) VALUES (?,?);";
+
+                connection.query(query, [name, dateAdded], (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.insertId);
+                })
+            });
+            return {
+                id: insertId,
+                name: name,
+                dateAdded: dateAdded
+            };
+            // return response;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    async deleteRowById(id) {
+        try {
+            id = parseInt(id, 10);
+        const response = await new Promise((resolve, reject) => {
+            const query = "DELETE FROM names WHERE id = ?";
+
+            connection.query(query, [id], (err, result) => {
+                if (err) reject(new Error(err.message));
+                resolve(result.affectedRows);
+            })
+        });
+
+        return response === 1 ? true : false;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+        
     }
 }
 
